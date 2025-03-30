de51105804f3b4c5a3902e415c7b4acbae4123f7
Goutham Shanubhogar <gouthamgouthu173@gmail.com>
2025-03-30 13:26:36 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://task-manager-six-beryl.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
